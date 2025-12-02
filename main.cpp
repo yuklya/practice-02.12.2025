6624eeac1ef89a68037b2dcf6545010c93c01724
yuklya <yuloseva@mail.ru>
2025-12-02 09:55:55 +0300

    dot

--- main.cpp
+++ main.cpp
@@ -13,17 +13,30 @@ namespace topit {
     virtual p_t next(p_t prev) const = 0;
     virtual ~IDraw() = default;
    };
-};
+  struct Dot: IDraw {
+    p_t begin() const override;
+    p_t next(p_t prev) const override;
+    p_t d;
+  };
+}
 int main() {
   using namespace topit; 
   p_t a{1, 1}, b{0, 1};
   std::cout << (a == b) << "\n";
-};
-
+}
+topit::p_t topit::Dot::begin() const {
+  return d;
+}
+topit:: p_t topit::Dot::next(p_t prev) const {
+  if (prev != d){
+    throw std::logic_error("bad prev");
+  }
+  return d;
+}
 bool topit:: operator == (p_t a, p_t  b) {
   return a.x == b.x && a.y == b.y;
-};
+}
 bool topit::operator!= (p_t a, p_t b) {
   return !(a == b);
-};
+}
 
