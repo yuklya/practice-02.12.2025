676fb00ecfc162485d6e54eb665a1e6bd7b19f4f
yuklya <yuloseva@mail.ru>
2025-12-02 09:41:20 +0300

    new program

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
-struct IDraw {
-  virtual p_t begin() const = 0; //Виртуальный метод, поэтому  равно 0
-  virtual p_t next(p_t p) const = 0;
-  virtual IDraw = default;
-}
+#include <iostream>
+namespace topit {
+  struct p_t {
+    int x, y;
+  };
+  struct f_t {
+    p_t aa, bb;
+  };
+  bool operator == (p_t a, p_t  b);
+  bool operator!= (p_t a, p_t b);
+};
+int main() {
+  using namespace topit; 
+  p_t a{1, 1}, b{0, 1};
+  std::cout << (a == b) << "\n";
+};
+
+bool topit:: operator == (p_t a, p_t  b) {
+  return a.x == b.x && a.y == b.y;
+};
+bool topit::operator!= (p_t a, p_t b) {
+  return !(a == b);
+};
+
